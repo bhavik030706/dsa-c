03cc199fdb9f38349fdef8936a96c75003d16369
Bhavik Singh <bhaviksingh1995@gmail.com>
2026-02-09 23:37:20 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+
+    
+    scanf("%s", str);
+
+    int left = 0;
+    int right = strlen(str) - 1;
+    char temp;
+
+    
+    while(left < right) {
+        temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+        left++;
+        right--;
+    }
+
+    
+    printf("%s", str);
+
+    return 0;
+}
